141a61f2a71934390208d3af4c25b6ecaf1545e0
Thimeth0013 <144969037+Thimeth0013@users.noreply.github.com>
2025-03-15 23:19:37 +0530

    Adding Booking Model

---
+++ backend/models/Booking.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const bookingSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    vehicleNo: {
+        type: String,
+        required: true
+    },
+    vehicleType: {
+        type: String,
+        required: true
+    },
+    serviceType: {
+        type: [String],
+        required: true
+    },
+    bookingDate: {
+        type: Date,
+        required: true
+    },
+    bookingTime: {
+        type: String,
+        required: true
+    },
+    phoneNumber: {
+        type: String,  // Storing phone number as string to handle different formats
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    mileage: {
+        type: Number,  // Storing mileage as a number
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['Active', 'Ongoing', 'Complete', 'OnHold', 'Canceled'],
+        default: 'Active', // Set default status to Active
+      }
+}, { timestamps: true });
+
+//mongoose.model("Booking") is the collection aka database
+const Booking = mongoose.model("Booking", bookingSchema);
+
+//Export Booking model to use in routes
+module.exports = Booking;
