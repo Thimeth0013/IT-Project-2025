a3868d325919bcf3bd6072e018ef24c45f4b239a
Thimeth0013 <144969037+Thimeth0013@users.noreply.github.com>
2025-03-18 08:04:13 +0530

    Updated customer web with Home and About pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import HomePage from './pages/Home';
+import CustomerLayout from './components/customerWebsite/CustomerLayout';
+import CustomerHome from './pages/cHome'
+import BookingForm from './components/customerWebsite/BookingForm';
+import SlotSelection from "./components/customerWebsite/SlotSelectBooking";
+import ViewList from './components/BookingList'
+import About from './pages/AboutC'
 
 function App() {
   return (
     <Router>
+      <CustomerLayout>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path='/' element={<CustomerHome/>} />
+        <Route path="/SlotSelectBooking" element={<SlotSelection />} />
+        <Route path='/BookingForm' element={<BookingForm/>}/>
+        <Route path='/About' element={<About/>}/>
       </Routes>
+      </CustomerLayout>
     </Router>
   );
 }
