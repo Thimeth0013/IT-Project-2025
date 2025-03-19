cf7dea6ff11262dc58f2d35df2b2b705eaaa68b6
hirunmayadunna <195747264+hirunmayadunna@users.noreply.github.com>
2025-03-19 16:30:38 +0530

    Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,16 @@
-<<<<<<< HEAD
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Customer Website
-import CustomerHome from './pages/CustomerWebsite/cHome'
+import HomePage from './pages/Home';
+// import CustomerLayout from './components/customerWebsite/CustomerLayout';
+// import CustomerHome from './pages/cHome'
 import BookingForm from './components/customerWebsite/BookingForm';
-import SlotSelection from "./components/customerWebsite/SlotSelectBooking";
-import ViewList from './components/BookingList'
-import About from './pages/CustomerWebsite/AboutC'
-import LoginC from './components/customerWebsite/LoginC';  // Your Login component
-import SignUpC from './components/customerWebsite/SignUpC'; // Your SignUp component
-
-// System Website
+// import SlotSelection from "./components/customerWebsite/SlotSelectBooking";
+// import ViewList from './components/BookingList'
+import CompanyOverview from './pages/AboutC'
 
 // import Supplier
-import ItemDashboard from './pages/SupplyManagement/Manager/ItemDashboard';
+import ItemDashboard from './pages/SupplyManagement/Manager/SuppliersItemDashboard';
 import SuppliersHomepage from './pages/SupplyManagement/Manager/SuppliersHomepage';
 import OrderDashboard from './pages/SupplyManagement/Manager/SuppliersOrderDashboard';
 import OrderForm from './pages/SupplyManagement/Manager/OrderForm';
@@ -29,23 +25,15 @@ import SupplierOrderDashboard from './pages/SupplyManagement/Supplier/SupplierOr
 // import SupplyDashboard from './components/Supply/SupplyDashboard';
 // import SupplyOrderList from './components/Supply/SupplyOrderList';
 
-// Import admin
-import AddUserForm from './components/systemWebsite/AddUserForm';
-
 function App() {
   return (
     <Router>
+      {/* <CustomerLayout> */}
       <Routes>
-        {/* Customer Website*/}
-        <Route path='/' element={<CustomerHome/>} />
-        <Route path="/SlotSelectBooking" element={<SlotSelection />} />
+        <Route path='/' element={<HomePage/>} />
+        {/* <Route path="/SlotSelectBooking" element={<SlotSelection />} /> */}
         <Route path='/BookingForm' element={<BookingForm/>}/>
-        <Route path='/About' element={<About/>}/>
-        <Route path="/LoginC" element={<LoginC />} /> {/* Login route */}
-        <Route path="/SignUpC" element={<SignUpC />} /> {/* SignUp route */}
-
-
-        {/* System Website*/}
+        <Route path='/About' element={<CompanyOverview/>}/>
 
         {/* Supplier */}
         <Route path="/suppliers" element={<SuppliersHomepage />} />
@@ -62,34 +50,10 @@ function App() {
         <Route path="/supply" element={<SupplierOrderDashboard />} />
         <Route path="/supply/supply" element={<SupplyDashboard />} />
         <Route path="/supply/supply/orders" element={<SupplyOrderList />} />
-
-        {/* Admin */}
-        <Route path="/AddUserForm" element={<AddUserForm/>} />
-
-=======
-import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import InventoryDashboard from "./components/InventoryDashboard";
-import Addstock from "./components/addstock";
-
-
-export default function App() {
-  return (
-     <Router>
-      <Routes>
-        {/* ✅ Redirect "/" to "/inventory" */}
-        <Route path="/" element={<Navigate to="/inventory" />} />
-        <Route path="/inventory" element={<InventoryDashboard />} />
-        <Route path="/add-stock" element={<Addstock />} />
-        {/* ❌ If no route matches, show a Not Found page */}
-        <Route path="*" element={<h2>Page Not Found </h2>} />
->>>>>>> 508a705 (Updated backend routes and added frontend module)
       </Routes>
+      {/* </CustomerLayout> */}
     </Router>
   );
 }
-<<<<<<< HEAD
 
 export default App;
-=======
->>>>>>> 508a705 (Updated backend routes and added frontend module)
