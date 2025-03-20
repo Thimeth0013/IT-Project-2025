69c6b1b7d94832e0821b677c8e915f85d0e4faef
hirunmayadunna <195747264+hirunmayadunna@users.noreply.github.com>
2025-03-20 12:03:33 +0530

    Updated Order Schema

--- backend/models/supplier/orders.js
+++ backend/models/supplier/orders.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 
 const OrderSchema = new mongoose.Schema({
     supplierID: mongoose.Schema.Types.ObjectId,
-    orderDetails: String,
     itemID: mongoose.Schema.Types.ObjectId,
+    orderDetails: String,
     quantity: Number,
     unitPrice: Number,
+    status: {
+        type: String,
+        enum: ['Processing', 'Accepted', 'Delivering', 'Completed', 'Declined', 'Undefined'],
+        default: 'Processing'
+    }
 });
 
 const OrderModel = mongoose.model('orders', OrderSchema);
